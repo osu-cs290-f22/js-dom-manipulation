8af806e79df2807704ba9e7e45a8fbbe11197622
Rob Hess <hessro@oregonstate.edu>
2022-10-25 15:23:32 -0700

    Add code from 10/25 lecture.

---
+++ access.js
@@ -0,0 +1,7 @@
+console.log("== document.body:", document.body)
+
+var body = document.body
+console.log("== body.childNodes:", body.childNodes)
+
+var nav = body.childNodes[3]
+console.log("== nav.parentNode:", nav.parentNode)
